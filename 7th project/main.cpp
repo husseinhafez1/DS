53cb87c3459bb0710a7fa9bfe2102b21245026f4
Robotwizard <49511689+husseinhafez1@users.noreply.github.com>
2022-03-17 23:38:21 +0200

    Code has some bugs will fix later

--- 7th project/main.cpp
+++ 7th project/main.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <math.h>
 
 template <typename T>
 struct Stack {
@@ -54,17 +56,130 @@ int top(Stack<T> *st,T *val){
     if(st->sp == -1)
         return 0;
     *val =  st->arr[st->sp];
-    return 0;
+    return 1;
 }
 
 template <typename T>
 int isempty(Stack<T> *st){
     if(st->sp == -1)
-        return 1;
+        return 0;
     return 1;
 }
 
+int Prec(char ch)
+{
+    switch (ch)
+    {
+    case '+':
+    case '-':
+        return 1;
+ 
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    }
+    return -1;
+}
+
+int isOperator(char c)
+{
+    if(c=='+'||c=='-'||c=='*'||c=='/'||c=='^')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
 int main() {
-    
+    char infix[256];
+    gets(infix);
+    Stack<char> operators;
+    Stack<double> res;
+    initStack<char>(&operators, strlen(infix));
+    initStack<double>(&res, strlen(infix));
+    char postfix[256];
+    int pfIndex = 0;
+    for (int i = 0; i < strlen(infix); i++) {
+        if (isdigit(infix[i])) {
+            postfix[pfIndex++] = infix[i];
+        }
+        else if (infix[i] == '(') {
+            push<char>(&operators, infix[i]);
+        }
+        else if (infix[i] == ')') {
+            char val;
+            top<char>(&operators, &val);
+            while (val != '(') {
+                postfix[pfIndex++] = val;
+                pop<char>(&operators, &val);
+                top<char>(&operators, &val);
+            }
+            pop<char>(&operators, &val);
+        }
+        else {
+            char val;
+            top<char>(&operators, &val);
+            while (!isempty(&operators) && Prec(infix[i]) <= Prec(val)) {
+                postfix[pfIndex++] = val;
+                pop<char>(&operators, &val);
+                top<char>(&operators, &val);
+            }
+            push<char>(&operators, infix[i]);
+        }
+    }
+    char val;
+    while (pop<char>(&operators, &val)) {
+        postfix[pfIndex++] = val;
+    }
+
+    postfix[pfIndex] = '\0';
+    printf("%s\n", postfix);
+
+    for (int i = 0; i < strlen(postfix); i++) {
+        if (isdigit(postfix[i])) {
+            push<double>(&res, postfix[i]-'0');
+        }
+        else {
+            if (postfix[i] == '+') {
+                double a, b;
+                pop<double>(&res, &a);
+                pop<double>(&res, &b);
+                push<double>(&res, a+b);
+            }
+            else if (postfix[i] == '-') {
+                double a, b;
+                pop<double>(&res, &a);
+                pop<double>(&res, &b);
+                push<double>(&res, a-b);
+            }
+            else if (postfix[i] == '*') {
+                double a, b;
+                pop<double>(&res, &a);
+                pop<double>(&res, &b);
+                push<double>(&res, a*b);
+            }
+            else if (postfix[i] == '/') {
+                double a, b;
+                pop<double>(&res, &a);
+                pop<double>(&res, &b);
+                push<double>(&res, b/a);
+            }
+            else if (postfix[i] == '^') {
+                double a, b;
+                pop<double>(&res, &a);
+                pop<double>(&res, &b);
+                push<double>(&res, pow(b,a));
+            }
+        }
+    }
+    double result;
+    pop(&res, &result);
+    printf("%lf", result);
     return 0;
 }
